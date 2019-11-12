611dbc81c0626ec804f1a3f1e6fc64f68d5a4f7f
Erik Moqvist <erik.moqvist@gmail.com>
2019-11-12 23:08:59 +0100

    Remove unused code.

--- src/hexdump/hexdump.h
+++ src/hexdump/hexdump.h
@@ -5,6 +5,5 @@
 #include <stdlib.h>
 
 char *nala_hexdump(const uint8_t *buffer, size_t size, size_t bytes_per_row);
-size_t nala_optimal_bytes_per_row(size_t element_size, size_t target, size_t range);
 
 #endif
