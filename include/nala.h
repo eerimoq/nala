a1559240f5eedb006e21644fb486277c8751748b
Erik Moqvist <erik.moqvist@gmail.com>
2019-11-13 19:41:21 +0100

    Version 0.15.0.

--- include/nala.h
+++ include/nala.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define VERSION "0.14.0"
+#define VERSION "0.15.0"
 
 #define TEST(name)                                      \
     void name(void);                                    \
