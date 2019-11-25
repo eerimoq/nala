dadc59acad6c8f431d148050b90361c3129411fb
Erik Moqvist <erik.moqvist@gmail.com>
2019-11-25 20:15:30 +0100

    Rename more parameters.

--- nala/dist/nala.h
+++ nala/dist/nala.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define VERSION "0.33.0"
+#define VERSION "0.35.0"
 
 #define TEST(name)                                      \
     void name(void);                                    \
