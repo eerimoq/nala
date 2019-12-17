d8ff26b0e2afcb39951b94cac68dffafdab91ccb
Erik Moqvist <erik.moqvist@gmail.com>
2019-12-17 08:46:59 +0100

    Version 0.66.0.

--- tests/files/test_collect_nala_mocks.h
+++ tests/files/test_collect_nala_mocks.h
@@ -1,7 +1,7 @@
 /*
 Mocks header file
 
-Generated with Nala version 0.63.0 (https://github.com/eerimoq/nala)
+Generated with Nala version 0.66.0 (https://github.com/eerimoq/nala)
 Do not edit manually
 */
 
