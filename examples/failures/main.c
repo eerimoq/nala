70ff1dd5bf9dbb1395ee21a7427e9c6c68572952
Erik Moqvist <erik.moqvist@gmail.com>
2020-01-18 09:18:31 +0100

    Add a wrong pointer test in the failure example.

--- examples/failures/main.c
+++ examples/failures/main.c
@@ -19,6 +19,17 @@ TEST(time_called_fewer_times_than_expected)
     ASSERT_EQ(time(NULL), 41);
 }
 
+TEST(time_wrong_pointer)
+{
+    time_t now;
+
+    time_mock_once(42);
+    time_mock_set_tloc_in_pointer(&now);
+
+    ASSERT_EQ(time(NULL), 42);
+    time(NULL);
+}
+
 TEST(segfault)
 {
     (*((int *)0)) = 1;
